84b2df1851edfb5b541f38bc828bed7f662233f1
Bruno De Bondt <mail@brunodbo.be>
2015-02-16 17:18:45 -0800

    JS structure; styling

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@
 
   $(function() {
     var tmd = $('#tmd'),
-        tmdToggle = $('#tmd-toggle'),
+        tmdOpen = $('#tmd-open'),
+        tmdClose = $('#tmd-close'),
         tmdPushFEl = $('.tmd-push-f'),
         tmdPushInnerEl = $('.tmd-push-inner'),
         tmdHideEl = $('.tmd-hide'),
@@ -16,67 +17,97 @@
         isPulled = 'is-pulled',
         // Hide / show
         isVisible = 'is-visible',
-        isHidden = 'is-hidden';
+        isHidden = 'is-hidden',
+        isAnimated = 'animated';
 
-    function openTmd() {
+    function openTmd(animated) {
       // Open TMD wrapper.
       if (tmd.hasClass(isClosed)) {
-        tmd.removeClass(isClosed);
+        $(this).removeClass(isClosed);
       }
       tmd.addClass(isOpen);
+      if (animated === true) {
+        tmd.addClass(isAnimated);
+      }
       // Push full width elements.
       if (tmdPushFEl.hasClass(isPulled)) {
-        tmdPushFEl.removeClass(isPulled);
+        $(this).removeClass(isPulled);
       }
       tmdPushFEl.addClass(isPushed);
+      if (animated === true) {
+        tmdPushFEl.addClass(isAnimated);
+      }
       // Push inner elements.
       if (tmdPushInnerEl.hasClass(isPulled)) {
-        tmdPushInnerEl.removeClass(isPulled);
+        $(this).removeClass(isPulled);
       }
       tmdPushInnerEl.addClass(isPushed);
+      if (animated === true) {
+        tmdPushInnerEl.addClass(isAnimated);
+      }
       // Hide elements.
       if (tmdHideEl.hasClass(isVisible)) {
-        tmdHideEl.removeClass(isVisible);
+        $(this).removeClass(isVisible);
       }
       tmdHideEl.addClass(isHidden);
+      if (animated === true) {
+        tmdHideEl.addClass(isAnimated);
+      }
     }
 
-    function closeTmd() {
+    function closeTmd(animated) {
       // Close TMD wrapper.
       if (tmd.hasClass(isOpen)) {
         tmd.removeClass(isOpen);
       }
       tmd.addClass(isClosed);
+      if (animated === true) {
+        tmd.addClass(isAnimated);
+      }
       // Pull back full width elements.
       if (tmdPushFEl.hasClass(isPushed)) {
         tmdPushFEl.removeClass(isPushed);
       }
       tmdPushFEl.addClass(isPulled);
+      if (animated === true) {
+        tmdPushFEl.addClass(isAnimated);
+      }
       // Pull back inner elements.
       if (tmdPushInnerEl.hasClass(isPushed)) {
         tmdPushInnerEl.removeClass(isPushed);
       }
       tmdPushInnerEl.addClass(isPulled);
+      if (animated === true) {
+        tmdPushInnerEl.addClass(isAnimated);
+      }
       // Show elements.
       if (tmdHideEl.hasClass(isHidden)) {
         tmdHideEl.removeClass(isHidden);
       }
       tmdHideEl.addClass(isVisible);
+      if (animated === true) {
+        tmdHideEl.addClass(isAnimated);
+      }
     }
 
     if ($.cookie('nat_tmr') === 'open') {
       openTmd();
+      tmdOpen.hide();
     }
-    tmdToggle.click(function() {
+    tmdOpen.click(function() {
       // console.log($.cookie('nat_tmr'));
-      if ($.cookie('nat_tmr') === 'undefined' || $.cookie('nat_tmr') === 'closed') {
-        openTmd();
+      // if ($.cookie('nat_tmr') === 'undefined' || $.cookie('nat_tmr') === 'closed') {
+        openTmd(true);
+        tmdOpen.hide();
         $.cookie('nat_tmr', 'open');
-      }
-      else if ($.cookie('nat_tmr') === 'open') {
-        closeTmd();
+      // }
+    });
+    tmdClose.click(function() {
+      // else if ($.cookie('nat_tmr') === 'open') {
+        closeTmd(true);
+        tmdOpen.show();
         $.cookie('nat_tmr', 'closed');
-      }
+      // }
     });
   });
 })(jQuery);
